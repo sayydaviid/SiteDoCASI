4a5626c0046b4f37f0cb55e552d6b4ea864e7939
sayydaviid <139977517+sayydaviid@users.noreply.github.com>
2025-03-05 17:53:04 -0300

    Mudanças no header.php
    Adicionei um dropdown no header.php e adicionei no CSS também

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,19 @@
         </div>
         <nav class="menu-container">
             <ul class="menu-container-op">
-                <li class="menu-item-list"><a href="./index.php">CASI</a></li>
+            <li class="menu-item-list dropdown">
+                    <a href="./index.php">CASI 
+                        <span class="dropdown-arrow">
+                            <svg class="ast-arrow-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="16px" height="10px" viewBox="57 35.171 26 16.043" enable-background="new 57 35.171 26 16.043" xml:space="preserve">
+                                <path d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z" fill="currentColor"/>
+                            </svg>
+                        </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Gestão Atual</a></li>
+                    </ul>
+                </li>
                 <li class="menu-item-list"><a href="./notices.php">Notícias</a></li>
                 <li class="menu-item-list"><a href="./projects.php">Projetos</a></li>
                 <li class="menu-item-list"><a href="./publications.php">Publicações</a></li>
